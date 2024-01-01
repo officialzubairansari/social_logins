f4723137cd58b1994ab5b326912fd8c14542741f
Zubair Ansari <officialzubairansari@gmail.com>
2024-01-01 16:24:17 +0500

    improvement

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('login', LoginController::class);
