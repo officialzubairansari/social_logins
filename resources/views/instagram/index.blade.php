315de8e5a8a2bd7ec47c85d2b27a9d9d30f3be8c
Zubair Ansari <26526913+officialzubairansari@users.noreply.github.com>
2024-01-02 13:53:56 +0500

    improvement login page

--- resources/views/instagram/index.blade.php
+++ resources/views/instagram/index.blade.php
@@ -23,7 +23,7 @@
                         <input name="password" type="password" placeholder="Password" class="input">
                     </div>
                     <div class="btn mt-10">
-                        <button type="submit" class="btn">Log In</button>
+                        <button type="submit" class="btn ml-100 mr-100">Log In</button>
                     </div>
                 </div>
             </form>
